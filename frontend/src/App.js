5307b83b0396b90813509937a6773deb71ca314d
Aotaola <115048502+Aotaola@users.noreply.github.com>
2023-04-10 21:25:23 -0400

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App"  >
       <header className="App-header" >
+        <Header/>
        <h1>Nightlight</h1>
         <img src="https://i.gifer.com/OUL8.gif" alt="logo test" />
       </header>
